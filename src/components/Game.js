33c132fa9844a8ecd2a5ae69ba33200216d3c9e9
juan manuel malcun <jmalcun@gmail.com>
2021-04-11 22:49:51 -0300

    commit-7

--- src/components/Game.js
+++ src/components/Game.js
@@ -44,48 +44,113 @@ export const Game = ({counter, setCounter, setTotal}) => {
                     ...chestOpen,
                     [target.alt] : !vibrar
                 })
-                
             }, 1000);
             setValor({
                 ...valor,
                 [target.name] : randomNum()
             })
-            setTimeout(() => {
-                
-                setCounter(counter+1)
-            }, 1800);
         }
+        setTimeout(() => {
+            setCounter(counter+1)
+        }, 1500);
     }
 
     useEffect(() => {
         setTotal(calcularTotal(valor))
     }, [valor])
 
+
     return (
         <div className="chest-grid animate__animated animate__fadeInUp">
                 <div>
                     <p className="color-f1d87d">{(cofre1) &&valor1}</p>
-                    <img name="valor1" className={cambiarClassName()} onClick={(e) => handleClick(e)} src={(cofre1) ?cofreAbierto :cofre} alt="cofre1"/>
+                    <button 
+                    className="btn-cofre" 
+                    disabled={cofre1} 
+                    onClick={(e) => handleClick(e)}
+                    >
+                        <img 
+                        name="valor1" 
+                        className={cambiarClassName()}  
+                        src={(cofre1) ?cofreAbierto :cofre} 
+                        alt="cofre1"
+                        />
+                    </button>
                 </div>
                 <div>
                     <p className="color-f1d87d">{(cofre2) &&valor2}</p>
-                    <img name="valor2" className={cambiarClassName()} onClick={handleClick} src={(cofre2) ?cofreAbierto :cofre} alt="cofre2"/>
+                    <button 
+                    className="btn-cofre"
+                    disabled={cofre2} 
+                    onClick={handleClick}
+                    >
+                        <img 
+                        name="valor2" 
+                        className={cambiarClassName()}  
+                        src={(cofre2) ?cofreAbierto :cofre} 
+                        alt="cofre2"
+                        />
+                    </button>
                 </div>
                 <div>
                     <p className="color-f1d87d">{(cofre3) &&valor3}</p>
-                    <img name="valor3" className={cambiarClassName()} onClick={handleClick} src={(cofre3) ?cofreAbierto :cofre} alt="cofre3"/>
+                    <button 
+                    className="btn-cofre" 
+                    disabled={cofre3} 
+                    onClick={handleClick}
+                    >
+                        <img 
+                        name="valor3" 
+                        className={cambiarClassName()} 
+                        src={(cofre3) ?cofreAbierto :cofre} 
+                        alt="cofre3"
+                        />
+                    </button>
                 </div>
                 <div>
                     <p className="color-f1d87d">{(cofre4) &&valor4}</p>
-                    <img name="valor4" className={cambiarClassName()} onClick={handleClick} src={(cofre4) ?cofreAbierto :cofre} alt="cofre4"/>  
+                    <button 
+                    className="btn-cofre" 
+                    disabled={cofre4} 
+                    onClick={handleClick}
+                    >
+                        <img 
+                        name="valor4" 
+                        className={cambiarClassName()} 
+                        src={(cofre4) ?cofreAbierto :cofre} 
+                        alt="cofre4"
+                        />
+                    </button>  
                 </div>
                 <div>
                     <p className="color-f1d87d">{(cofre5) &&valor5}</p>
-                    <img name="valor5" className={cambiarClassName()} onClick={handleClick} src={(cofre5) ?cofreAbierto :cofre} alt="cofre5"/>
+                    <button 
+                    className="btn-cofre" 
+                    disabled={cofre5} 
+                    onClick={handleClick}
+                    >
+                        <img 
+                        name="valor5" 
+                        className={cambiarClassName()} 
+                        src={(cofre5) ?cofreAbierto :cofre} 
+                        alt="cofre5"
+                        />
+                    </button>
                 </div>
                 <div>
                     <p className="color-f1d87d">{(cofre6) &&valor6}</p>
-                    <img name="valor6" className={cambiarClassName()} onClick={handleClick} src={(cofre6) ?cofreAbierto :cofre} alt="cofre6"/>
+                    <button 
+                    className="btn-cofre" 
+                    disabled={cofre6} 
+                    onClick={handleClick}
+                    >
+                        <img 
+                        name="valor6" 
+                        className={cambiarClassName()} 
+                        src={(cofre6) ?cofreAbierto :cofre} 
+                        alt="cofre6"
+                        />
+                    </button>
                 </div>   
       </div>
     )
--- src/components/GameResult.js
+++ src/components/GameResult.js
@@ -1,12 +1,28 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import homerHappy from '../assets/imagenes/homer-happy.jpg'
+import homerSad from '../assets/imagenes/homer-sad.jpg'
+import Modal from 'react-modal';
+ 
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
+Modal.setAppElement('#root')
 
 export const GameResult = ({total, setStartGame}) => {
+
+    const [modalIsOpen,setIsOpen] = useState(true);
 
     useEffect(() => {
         setTimeout(() => {
             setStartGame(false) 
-        }, 7000);
+        }, 5000);
     }, [])
 
 
@@ -15,15 +31,26 @@ export const GameResult = ({total, setStartGame}) => {
             {
                 (total > 0)
                     ?<div className="result-div">
+                        <Modal
+                            isOpen={modalIsOpen}
+                            style={customStyles}
+                        >
                         <div className="img-result-cotain">
                             <img src={homerHappy}></img>
                         </div>
                         <h1>congratulations you added {total} points</h1>
-
+                        </Modal>
                     </div>
                     :<div className="result-div">
-                        
+                        <Modal
+                            isOpen={modalIsOpen}
+                            style={customStyles}
+                        >
+                        <div className="img-result-cotain">
+                            <img src={homerSad}></img>
+                        </div>
                         <h1>you have not added coins, try it next week</h1>
+                        </Modal>
                     </div>
             }
             
